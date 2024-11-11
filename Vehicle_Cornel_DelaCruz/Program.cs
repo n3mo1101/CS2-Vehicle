c8fa7329fee7332cae8c7b86f64ff6d72997ea57
Grace Cornel <202380082@psu.palawan.edu.ph>
2024-11-11 10:03:21 +0800

    Base Class and Derived Class Vehicle

---
+++ Vehicle_Cornel_DelaCruz/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle_Cornel_DelaCruz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+
+        class Vehicle 
+        { 
+            public string VehicleId { get; set; }
+            public string Make {  get; set; }
+            public string Model { get; set; }
+            public float FuelEfficiency { get; set; }
+
+            public Vehicle (string vehicleid, string make, string model, float fuelefficiency) 
+            {
+                VehicleId = vehicleid;
+                Make = make;
+                Model = model;
+                FuelEfficiency = fuelefficiency;
+            }
+        }
+
+        class Car : Vehicle
+        {
+            public bool NumDoors;
+            public bool IsAutomatic;
+
+            public Car(string vehicleid, string make, string model, float fuelefficiency, 
+                bool numdoors, bool isautomatic) :
+                base(vehicleid, make, model, fuelefficiency)
+            {
+                NumDoors = numdoors;
+                IsAutomatic = isautomatic;
+            }
+
+            public float CalculateFuelConsumption (float distance) 
+            {
+                return distance / FuelEfficiency;
+            }
+        }
+
+        class Motorcycle : Vehicle
+        {
+
+        }
+
+    }
+}
